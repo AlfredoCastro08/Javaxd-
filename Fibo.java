37c9e45476be1d55bdb933596efd513ea5dcf85c
AlfredoCastro08 <45361748+AlfredoCastro08@users.noreply.github.com>
2019-02-24 12:54:51 -0600

    Correción de un ; >:v

--- Fibo.java
+++ Fibo.java
@@ -4,65 +4,66 @@ public class Fibo{
   public static void main(String args[]){
 
     //Variables 
-    int a = 0, b = 1, c = 0, i = 0;
+    int Num1 = 0, Num2 = 1, Temp = 0, i = 0;
     	     
-    System.out.println("Serie con For: ");
+    System.out.println("Serie con for: ");
     
     for(i=0; i<10; i++){
      if(i<9){
   	
-	System.out.print(a + ",");
-	c = a + b;
-	a = b;
-	b = c;
+	System.out.print(Num1 + ",");
+	Temp = Num1 + Num2;
+	Num1 = Num2;
+	Num2 = Temp;
       
       }//<-Fin if  
       else{
-       System.out.print(a);
+       System.out.print(Num1);
       }//<- Fin else 
     }//<- Fin for
 
     System.out.println("");
     i=0; 
-    a=0;   //Regresar todas las variables a su inicio para evitar errores
-    b=1;
-    c=0; 
+    Num1=0;   //Regresar todas las variables a su inicio para evitar errores
+    Num2=1;
+    Temp=0; 
     
     System.out.println("Serie con while");
     
     while(i<10){
      if(i<9){
        
-	System.out.print(a + ",");
-	c = a + b;
-	a = b;
-	b = c;
+	System.out.print(Num1 + ",");
+	Temp = Num1 + Num2;
+	Num1 = Num2;
+	Num2 = Temp;
       
      }//<-Fin if
       else{
-       System.out.print(a);
+       System.out.print(Num1);
       }//<- Fin else
        i++;
     }//<- Fin while	
 
     System.out.println("");
-    i=0;
-    a=0;
-    b=1;
-    c=0;
+    i=0; 
+    Num1=0;   //Regresar todas las variables a su inicio para evitar errores
+    Num2=1;
+    Temp=0; 
+    
     System.out.println("Serie con do-while");
 
    do{
        if(i<9){
        
-	System.out.print(a + ",");
-	c = a + b;
-	a = b;
-	b = c;
+	System.out.print(Num1 + ",");
+	Temp = Num1 + Num2;
+	Num1 = Num2;
+	Num2 = Temp;
       
      }//<-Fin if
       else{
-       System.out.print(a);
+       System.out.print(Nun1);
       }//<- Fin else
        i++;
 	 	
