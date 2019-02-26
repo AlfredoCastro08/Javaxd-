e2201c134dfb672d3c80b639047beeaa4377580f
AlfredoCastro08 <45361748+AlfredoCastro08@users.noreply.github.com>
2019-02-26 12:30:57 -0600

    Se corrigió las variables de int a double
    También sus respectivas conversiones

--- Ticket.java
+++ Ticket.java
@@ -1,25 +1,22 @@
-import java.util.Scanner;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Ticket {
 
 	public static void main(String[] args) {
 	 
-		//Objeto entrada
-		Scanner Entrada = new Scanner(System.in);
 		
-		//Variables
-		String Producto, Cantidad, Precio; 
-		int Cantidad1 = 0, Precio2=0, j=0, i=0, Total=0, Subtotal=0, IVA=0, Pago=0, Cambio=0, TotalaPagar=0;
+		
+	    //Variables
+	    String Producto, Cantidad, Precio; 
+	    double Cantidad1=0, Precio1=0, j=0, i=0, Total=0, Subtotal=0, IVA=0, Pago=0, Cambio=0, SubTotal=0, TotalaPagar=0;
 
-		System.out.println("GRACIAS POR SU COMPRA. VUELVA PRONTO.");
+	    System.out.println("GRACIAS POR SU COMPRA. VUELVA PRONTO.");
 	    System.out.println("--------------------------------------------");
 	    System.out.println("");
 	    System.out.println("Prodcuto"+"      "+"Cantidad"+"      "+"Precio"+"     "+"Total");
 	    System.out.println("");
 		
-	    //Petición de datos con JOptionPane
+	    //PeticiÃ³n de datos con JOptionPane
 	    Producto = JOptionPane.showInputDialog("Ingresa el nombre del producto");
 	    System.out.print(Producto);
 	    Cantidad = JOptionPane.showInputDialog("Ingresa la cantidad del producto");
@@ -27,11 +24,11 @@ public class Ticket {
 	    Precio = JOptionPane.showInputDialog("Ingresa el precio del producto");
 	    System.out.print("            "+Precio);
 	   
-	   //Converción de las cadenas a enteros 
-	    Cantidad1 = Integer.parseInt(Cantidad);
-	    Precio2 = Integer.parseInt(Precio);
+	    //ConversiÃ³n de las cadenas a double 
+	    Cantidad1 = Double.parseDouble(Cantidad);
+	    Precio1 = Double.parseDouble(Precio);
 	    
-	    Total = Cantidad1*Precio2;
+	    Total = Cantidad1*Precio1;
 	    System.out.print("          "+Total);
 	    System.out.println("");
 	    
