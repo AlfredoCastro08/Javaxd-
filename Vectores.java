764874ec41d8cf7335cd639f6983352837db6a49
AlfredoCastro08 <45361748+AlfredoCastro08@users.noreply.github.com>
2019-03-05 16:40:08 -0600

    Vectores
    Pequeño programa de un vector con 5 posiciones

---
+++ Vectores.java
@@ -0,0 +1,23 @@
+public class Vectores{
+
+  public static void main(String args[]){
+
+	//Variables
+	int Numeros[] = new int[5];
+
+	//Asignar de manera estática un valor a cada posición
+	Numeros[0] = 5;
+	Numeros[1] = 12;
+	Numeros[2] = 15;
+	Numeros[3] = 896;
+	Numeros[4] = 4242;
+
+	//Los corchetes son para que se muestren en pantalla :v[21]
+	System.out.println("[" + Numeros[0] + "]");
+	System.out.println("[" + Numeros[1] + "]");
+	System.out.println("[" + Numeros[2] + "]");
+	System.out.println("[" + Numeros[3] + "]");
+	System.out.println("[" + Numeros[4] + "]");
+ 
+  }
+}
